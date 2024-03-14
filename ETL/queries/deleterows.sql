4e39c06cc6bc66ee8cc241f30f0992c1f8c2a692
ChristopherLagos <47311064+ChristopherLagos@users.noreply.github.com>
2024-03-14 15:33:45 -0400

    Update deleterows.sql
    Added delete row queries for room_unavailable

--- ETL/queries/deleterows.sql
+++ ETL/queries/deleterows.sql
@@ -3,3 +3,13 @@ delete from chain where chid is NULL or '';
 delete from hotel where hid is NULL or '';
 
 delete  from client where clid is null or '';
+
+-- room_unavailable
+
+delete from room_unavailable where ruid is null;
+
+delete from room_unavailable where rid is null;
+
+delete from room_unavailable where start_date is null or start_date='';
+
+delete from room_unavailable where end_date is null or end_date='';
