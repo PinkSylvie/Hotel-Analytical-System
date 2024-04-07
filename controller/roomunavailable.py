eb08f4aa9dba2421bb4b3ef7592a494538b9de2f
ChristopherLagos <47311064+ChristopherLagos@users.noreply.github.com>
2024-04-07 04:39:12 -0400

    Added roomunavailable.py via upload
    This is the controller for roomunavailable which contains the RoomUnavailable class with the following functions:
    - make_json(tuples) – returns json for multiple tuples
    - make_json_one(tuple) – returns json for a single tuple
    - getAllRoomUnavailable() – returns json with all the data in roomunavailable
    - addNewRoomUnavailable(data) – adds a new tuple with the data provided to roomunavailable
    - getRoomUnavailableByRuid(ruid) – returns json with one tuple corresponding the ruid provided
    - updateRoomUnavailableByRuid(ruid, data) – updates the data in a tuple by ruid in roomunavailable
    - deleteRoomUnavailableByRuid(ruid) – deletes a tuple in roomunavailable by ruid

---
+++ controller/roomunavailable.py
@@ -0,0 +1,77 @@
+from model.roomunavailable import RoomUnavailableDAO
+from flask import jsonify
+
+class RoomUnavailable:
+    def make_json(self, tuples):
+        result = []
+        for t in tuples:
+            D = {}
+            D['ruid'] = t[0]
+            D['rid'] = t[1]
+            D['startdate'] = t[2]
+            D['enddate'] = t[3]
+            result.append(D)
+        return result
+    
+    def make_json_one(self, tuple):
+        print("printing tuple", tuple)
+        D = {}
+        D['ruid'] = tuple[0]
+        D['rid'] = tuple[1]
+        D['startdate'] = tuple[2]
+        D['enddate'] = tuple[3]
+        return D
+    
+    def getAllRoomUnavailable(self):
+        model = RoomUnavailableDAO()
+        result = model.getAllRoomUnavailable()
+        answer = self.make_json(result)
+        return answer
+    
+    def addNewRoomUnavailable(self, data):
+        rid = data['rid']
+        startdate = data['startdate']
+        enddate = data['enddate']
+        dao = RoomUnavailableDAO()
+        ru = dao.addNewRoomUnavailable(rid, startdate, enddate)
+        result = self.make_json(ru)
+        return result 
+    
+    def getRoomUnavailableByRuid(self, ruid):
+        dao = RoomUnavailableDAO()
+        ru = dao.getRoomUnavailableByRuid(ruid)
+        if not ru:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.make_json_one(ru)
+            return result
+    
+    def updateRoomUnavailableByRuid(self, ruid, data):
+        dao = RoomUnavailableDAO()
+        ru = dao.updateRoomUnavailableByRuid(ruid, data)
+        if not ru:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.make_json_one(ru)
+            return result
+        
+    def deleteRoomUnavailableByRuid(self, ruid):
+        dao = RoomUnavailableDAO()
+        ru = dao.deleteRoomUnavailableByRuid(ruid)
+        if not ru:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.make_json(ru)
+            return result
+        
+    def getByRid(self, rid):
+        dao = RoomUnavailableDAO()
+        ru = dao.getByRid(rid)
+        if not ru:
+            return jsonify("Not Found"), 404
+        else:
+            result = self.make_json_one(ru)
+            return result
+    
+    
+    
